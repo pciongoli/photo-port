38f89f9dd5cec9dc75fbbfa6158007819df88b7a
pcion <patrick.ciongoli@gmail.com>
2022-05-12 14:46:28 -0400

    created about

---
+++ src/components/Nav/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+function Nav() {
+   // define categories in an array
+   const categories = [
+      {
+         name: "commercial",
+         description:
+            "Photos of grocery stores, food trucks, and other commercial projects",
+      },
+      {
+         name: "portraits",
+         description: "Portraits of people in my life",
+      },
+      {
+         name: "food",
+         description: "Delicious delacacies",
+      },
+      {
+         name: "landscape",
+         description:
+            "Fields, farmhouses, waterfalls, and the beauty of nature",
+      },
+   ];
+
+   // define the onClick function
+   function categorySelected() {
+      console.log("hello");
+   }
+
+   return (
+      <header>
+         <h2>
+            <a href="/">
+               <span role="img" aria-label="camera">
+                  📸
+               </span>
+               Oh Snap!
+            </a>
+         </h2>
+         <nav>
+            <ul className="flex-row">
+               <li className="mx-2">
+                  <a href="#about">About me</a>
+               </li>
+               <li>
+                  <span>Contact</span>
+               </li>
+               {categories.map((category) => (
+                  <li className="=mx-1" key={category.name}>
+                     <span onClick={() => categorySelected}>
+                        {category.name}
+                     </span>
+                  </li>
+               ))}
+            </ul>
+         </nav>
+      </header>
+   );
+}
+
+export default Nav;
