1492ad884d61dc169199e4bb61c33325b746cdc0
pcion <patrick.ciongoli@gmail.com>
2022-05-13 23:02:43 -0400

    added contact component

---
+++ src/components/Contact/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ContactForm() {
+   return (
+      // JSX
+      <section>
+         <h1>Contact me</h1>
+         <form id="contact-form">
+            {/* name input */}
+            {/* email input */}
+            {/* message text area */}
+         </form>
+      </section>
+   );
+}
+
+// name of function needs to be in export statement
+export default ContactForm;
