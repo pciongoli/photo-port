8a35645adce818ed5048535a63c275f7b3519d96
pcion <patrick.ciongoli@gmail.com>
2022-05-13 23:09:23 -0400

    completed markup structure for contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,8 +7,22 @@ function ContactForm() {
          <h1>Contact me</h1>
          <form id="contact-form">
             {/* name input */}
+            <div>
+               <label htmlFor="name">Name:</label>
+               <input type="text" name="name" />
+            </div>
             {/* email input */}
+            <div>
+               <label htmlFor="email">Email address:</label>
+               <input type="email" name="email" />
+            </div>
             {/* message text area */}
+            <div>
+               <label htmlFor="message">Message:</label>
+               <textarea name="message" rows="5" />
+            </div>
+            {/* button */}
+            <button type="submit">Submit</button>
          </form>
       </section>
    );
